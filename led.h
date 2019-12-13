666722566a131841b34355fbdb9fb7e84a5211e6
shm9599 <56909824+shm9599@users.noreply.github.com>
2019-12-13 20:07:03 +0900

    Create led.h

---
+++ led.h
@@ -0,0 +1,12 @@
+#ifndef _LED_H_
+#define _LED_H_
+int ledLibInit(void);
+int ledOnOff(int ledNum,int ledOnOff);
+int ledStatus (void);
+int ledLidExit(void);
+
+#define LED_DRIVER_NAME         "dev/periled"
+
+
+#endif _LED_H_
+
